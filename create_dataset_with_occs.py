f13a630ab737f5863dfc4ca8b33c74a1ab3aefba
galberding <galberding@techfak.uni-bielefeld.de>
2019-07-27 17:16:53 +0200

    Qubes, Pens and spheres can be generated, the amount of generated samples as well as the path can be adjusted, gen_dataset automatically generates a train and test set

--- create_dataset_with_occs.py
+++ create_dataset_with_occs.py
@@ -1,4 +1,4 @@
-from main import generate_cloud_pen, generate_cloud_sphere, generate_cloud_cube, transform_cloud, cloud2voxel
+from VoxelView.main import generate_cloud_pen, generate_cloud_sphere, generate_cloud_cube, transform_cloud, cloud2voxel
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from time import gmtime, strftime
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import os
 from pyquaternion import Quaternion
 from scipy.spatial import Delaunay
+import math
 
 voxelSpaceSize = 16
 voxelRange = 2.0
@@ -14,28 +15,32 @@ voxelRange = 2.0
 # How many occ/points should be generated
 point_samples = 100000
 
+working_path = "dataset"
+
 # Paths to datasets
 qube_path = "dataset/qube/"
 sphere_path = "dataset/sphere/"
 pen_path = "dataset/pen/"
+
 
 # range_quat_cube = 45
 range_quat_cube = 5
 range_quat_pen = 20 #* 10
 
 
-def create_dataset_dir():
+def create_dataset_dir(path):
     '''
-    Check if necessary files are existent.
-    If not the missing directories will be created.
+    Create the dataset for the specific datapoints.
+    It will create directories for train and test.
+    :param path:
     :return:
     '''
-    if not os.path.exists(qube_path):
-        os.makedirs(qube_path)
-    if not os.path.exists(sphere_path):
-        os.makedirs(sphere_path)
-    if not os.path.exists(pen_path):
-        os.makedirs(pen_path)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    if not os.path.exists(path+"train/"):
+        os.makedirs(path+"train/")
+    if not os.path.exists(path+"test/"):
+        os.makedirs(path+"test/")
 
 
 def gen_points(cloud, samples):
@@ -81,27 +86,11 @@ def safe_sample(points, occ, voxel, path):
 
 
 # Helper function to generate occs and points as well as returning the correct path to store the samples
-def qube(cloud):
+
+def occ_generation(cloud):
     points, occs = gen_points(cloud, point_samples)
     voxel = cloud2voxel(cloud, voxelRange, size=voxelSpaceSize)
-
-    # print("done!")
-    return (points, occs, voxel, qube_path)
-
-
-def sphere(cloud):
-    points, occs = gen_points(cloud, point_samples)
-    voxel = cloud2voxel(cloud, voxelRange, size=voxelSpaceSize)
-    # print("done!")
-    return (points, occs, voxel, sphere_path)
-
-
-def pen(cloud):
-    points, occs = gen_points(cloud, point_samples)
-    voxel = cloud2voxel(cloud, voxelRange, size=voxelSpaceSize)
-    # print("done!")
-    return (points, occs, voxel, pen_path)
-
+    return points, occs, voxel, working_path
 
 # Parallelizes the generation of occupancies.
 # This is done by testing 100000 if they are inside the convex hull of the previous generated Pintcloud
@@ -120,19 +109,13 @@ def gen_samples(generator, config_list):
     for res in results:
         safe_sample(*res)
 
-
-if __name__ == '__main__':
-
-    create_dataset_dir()
-
-
-    # qube
+def qube_generation_process(samples):
     for shapeSize in [6, 7,8, 9]:
         dimension = voxelRange * shapeSize / voxelSpaceSize
         cloud = generate_cloud_cube(size=voxelSpaceSize, dimension=dimension)
 
         conf_list = []
-        for i in range(range_quat_cube):
+        for i in range(samples):
             quat = Quaternion.random()
             if i == 0: quat = Quaternion()  # first quat = 1 + 0i + 0j + 0k
             if i % 100 == 0: print(strftime("%H%M%S", gmtime()), 'cube', '\t', 'shapeSize', shapeSize, '\t', 'i', i,
@@ -140,34 +123,73 @@ if __name__ == '__main__':
                                    'quat', quat)
             transformed = transform_cloud(cloud, quat)
             conf_list.append(transformed)
-        gen_samples(qube, conf_list)
-
-    # # Sphere
-    # transf_spheres = []
-    # for k in range(7):
-    #     for shapeSize in [8, 9, 10, 11, 12, 13, 14, 15, 16]:
-    #         dimension = voxelRange * shapeSize / voxelSpaceSize
-    #         cloud = generate_cloud_sphere(size=voxelSpaceSize, dimension=dimension)
-
-    #         quat = Quaternion.random()
-    #         cloud_transformed = transform_cloud(cloud, quat)  #
-
-    #         # voxel = cloud2voxel(cloud_transformed, voxelRange, size=voxelSpaceSize)
-    #         transf_spheres.append(cloud_transformed)
-    # gen_samples(sphere, transf_spheres)
+        gen_samples(occ_generation, conf_list)
 
 
-#    # # Pen
-#  for shapeSize in [8, 9, 10, 11, 12, 13, 14, 15, 16]:
-#       dimension = voxelRange * shapeSize / voxelSpaceSize
-#        cloud = generate_cloud_pen(size=voxelSpaceSize, dimension=dimension)
-#        conf_list = []
-#        for i in range(range_quat_pen):
-#            quat = Quaternion.random()
-#            if i == 0: quat = Quaternion()  # first quat = 1 + 0i + 0j + 0k
-#            if i % 100 == 0: print(strftime("%H%M%S", gmtime()), 'cube', '\t', 'shapeSize', shapeSize, '\t', 'i', i,
-#                                   '\t',
-#                                   'quat', quat)
-#            transformed = transform_cloud(cloud, quat)
-#            conf_list.append(transformed)
-#        gen_samples(pen, conf_list)
+def sphere_generation_process(samples):
+    transf_spheres = []
+    for k in range(samples):
+        for shapeSize in [8, 9, 10, 11]:
+            dimension = voxelRange * shapeSize / voxelSpaceSize
+            cloud = generate_cloud_sphere(size=voxelSpaceSize, dimension=dimension)
+
+            quat = Quaternion.random()
+            cloud_transformed = transform_cloud(cloud, quat)  #
+
+            # voxel = cloud2voxel(cloud_transformed, voxelRange, size=voxelSpaceSize)
+            transf_spheres.append(cloud_transformed)
+    gen_samples(occ_generation, transf_spheres)
+
+
+def pen_generation_process(samples):
+    for shapeSize in [8, 9, 10, 11]:
+        dimension = voxelRange * shapeSize / voxelSpaceSize
+        cloud = generate_cloud_pen(size=voxelSpaceSize, dimension=dimension)
+        conf_list = []
+        for i in range(samples):
+            quat = Quaternion.random()
+            if i == 0: quat = Quaternion()  # first quat = 1 + 0i + 0j + 0k
+            if i % 100 == 0: print(strftime("%H%M%S", gmtime()), 'cube', '\t', 'shapeSize', shapeSize, '\t', 'i', i,
+                                   '\t',
+                                   'quat', quat)
+            transformed = transform_cloud(cloud, quat)
+            conf_list.append(transformed)
+        gen_samples(occ_generation, conf_list)
+
+
+def gen_dataset(voxel_model, path, samples, voxel_space_size=16, voxel_range=2.0):
+    '''
+    Generate the voxel qube dataset.
+    :param voxel_model (String): qube | sphere | pen
+    :param path (String): note: path needs to end with "/"
+    :param samples (int): amount of (training) samples to generate (actual sample size will be 4*samples)
+        The test set will be 33% as big as the train set.
+    :return:
+    '''
+    global voxelSpaceSize
+    voxelSpaceSize = voxel_space_size
+    global voxelRange
+    voxelRange = voxel_range
+    create_dataset_dir(path)
+    train_path = path+"train/"
+    test_path = path+"test/"
+    global working_path
+    working_path = train_path
+    if voxel_model is "qube":
+        qube_generation_process(samples)
+    elif voxel_model is "sphere":
+        sphere_generation_process(samples)
+    elif voxel_model is "pen":
+        pen_generation_process(samples)
+
+    working_path = test_path
+    if voxel_model is "qube":
+        qube_generation_process(math.ceil(samples * 0.33))
+    elif voxel_model is "sphere":
+        sphere_generation_process(math.ceil(samples * 0.33))
+    elif voxel_model is "pen":
+        pen_generation_process(math.ceil(samples * 0.33))
+
+
+if __name__ == '__main__':
+    gen_dataset("pen", "dataset/pen/", 3)
